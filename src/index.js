6d018d748a89f2ff26f662be9d9515a73a3fa1c2
John Dunning <fw@johndunning.com>
2022-01-25 14:05:51 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const ipc = require("electron").ipcRenderer;
+const FitAddon = require('xterm-addon-fit').FitAddon;
+
+const term = new Terminal();
+const fitAddon = new FitAddon();
+let resizeTimer;
+
+term.loadAddon(fitAddon);
+term.open(document.getElementById('terminal'));
+
+ipc.on("terminal.incomingData", (
+	event,
+	data) => {
+	term.write(data);
+});
+
+term.onData(e => {
+	ipc.send("terminal.keystroke", e);
+});
+
+fitAddon.fit();
+
+window.addEventListener("resize", () => {
+	clearTimeout(resizeTimer);
+	setTimeout(() => fitAddon.fit(), 250);
+});
